b035d0462d940de1d382910ec56cdc170c7214ae
Reinner Steven Daza Leiva <reinnerdazaleiva@outlook.com>
2021-02-28 22:48:00 -0500

    Inicial ESP32 Wifit

---
+++ README.md
@@ -0,0 +1,19 @@
+# ESP32 WiFi
+
+Este es un ejemplo de como se realiza una conexion a WiFi por medio del microcontrolador ESP32 o ESP32OLED para aplicaciones IOT
+
+# Descripción
+Este es un proyecto para el microcontrolado ESP32 o ESP32OLED, fue desarrollado medienta el plugin de platformio para Visual Studio Code.
+Este proyecto es compatible con las librerias para Arduino del ESP32. Se recomienda utilizar el plugin de platformio para VSC, ya que cuenta con mayor compatibilidad.
+Si desea conocer mas de platformio, ingrese al siguiente [enlace](https://platformio.org).
+
+Este proyecto se encarga de validar como se conecta a una red WiFi. Tambien dar ejemplos de la libreria utilizada
+
+## Instalación
+
+Para poder ejecutar el ejemplo de MQTT, realizar los siguientes pasos
+1. Instalacion de Visual Studio Code
+2. Clonar el repositorio
+3. Agregar las librerias utilizada. para visualizar las librerias utilizadas, ver el archivo platformio.ini, en este se encuentra las librerias utilizadas en el proyecto
+4. Actualizar credenciales para el aplicativo
+5. Compilar en una ESP32
---
+++ src/main.cpp
@@ -0,0 +1,123 @@
+/**
+ * Incluir librerias
+ * */
+#include <Arduino.h>
+#include <WiFi.h>
+#include "SSD1306Wire.h"
+
+/**
+ * Definir variables
+ * */
+#define LED_OUTPUT 12
+
+
+/**
+ * Variables para WiFi
+ * */
+const char *ssid = "ssid";
+const char *password = "password";
+
+//****************************************
+//******* DECLARACION DE FUNCIONES *******
+//****************************************
+
+void scanNetworkWiFi();
+void connectNetworkWiFi();
+String translateEncryptionType(wifi_auth_mode_t encryptionType);
+
+
+//****************************************
+//**************** SETUP *****************
+//****************************************
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(LED_OUTPUT, OUTPUT);
+}
+
+//****************************************
+//**************** LOOP ******************
+//****************************************
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_OUTPUT, HIGH);
+  delay(2000);
+  digitalWrite(LED_OUTPUT, LOW);
+  delay(2000);
+
+  if (WiFi.isConnected())
+  {
+    Serial.println("Connected WiFi");
+    delay(2000);
+    Serial.println(WiFi.localIP().toString());
+  }
+  else
+  {
+    // Demostracion de scan
+    scanNetworkWiFi();
+    // Conectar a una red predeterminada
+    connectNetworkWiFi();
+  }
+}
+
+
+//****************************************
+//******* DEFINICIÓN FUNCIONES  **********
+//****************************************
+
+/**
+ * Funcion que se encarga de indicar que tipo de red se detecto
+ * */
+String translateEncryptionType(wifi_auth_mode_t encryptionType)
+{
+  switch (encryptionType)
+  {
+  case (0):
+    return "Open";
+  case (1):
+    return "WEP";
+  case (2):
+    return "WPA_PSK";
+  case (3):
+    return "WPA2_PSK";
+  case (4):
+    return "WPA_WPA2_PSK";
+  case (5):
+    return "WPA2_ENTERPRISE";
+  default:
+    return "UNKOWN";
+  }
+}
+
+/**
+ * Funcion que se encarga de escanear las redes para ESP32
+ * */
+void scanNetworkWiFi()
+{
+  int numberOfNetworks = WiFi.scanNetworks();
+  Serial.println("Result scan: " + (String) numberOfNetworks);
+  for (int i = 0; i < numberOfNetworks; i++)
+  {
+    String encryptionTypeDescription = translateEncryptionType(WiFi.encryptionType(i));
+    //String nameNetwork = WiFi.SSID(i) + "\n" + WiFi.RSSI(i) + "\n" + WiFi.channel(i)  + "\n" + encryptionTypeDescription;
+    String nameNetwork = WiFi.SSID(i) + "\n" + encryptionTypeDescription;
+    Serial.println(nameNetwork);
+    delay(3000);
+  }
+}
+
+/**
+ * Funcion que se encarga de conextarse a una red WiFi
+ * */
+void connectNetworkWiFi()
+{
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.println("Estado Wifi" + WiFi.status());
+  }
+}
+
